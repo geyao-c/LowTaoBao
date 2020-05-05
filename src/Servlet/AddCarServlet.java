ee1fa3705eea486b8d4a68df78ecb6154b0f7b2d
JameyWoo <2622075127wjh@gmail.com>
2020-05-05 13:11:29 +0800

    完成项目

--- src/Servlet/AddCarServlet.java
+++ src/Servlet/AddCarServlet.java
@@ -4,6 +4,7 @@ import Entity.Goods;
 import Entity.ShopCar;
 import Service.GoodsService;
 import Service.ShopCarService;
+import com.alibaba.fastjson.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,26 +24,49 @@ public class AddCarServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
 
         String userid = request.getSession().getAttribute("userid").toString();
+
+        System.out.println("before add car");
+
         //获得前端传送来的所有商品ID
-        String []goodsid = request.getParameterValues("items");
+        String goodsid = request.getParameter("items");
         ShopCarService shopCarService = new ShopCarService();
         GoodsService goodsService = new GoodsService();
 
+        System.out.println("iter the items");
         System.out.println(goodsid);
+        String[] goodIds = goodsid.split(",");
+        System.out.println("goodIds: " + goodIds);
 
-        for(String goodid : goodsid){
+        // TODO: 添加错误, 无法解析列表
+
+        JSONObject object = new JSONObject();
+        boolean flag1 = false, flag2 = true;
+
+        for (String goodid : goodIds){
+            System.out.println("GoodId: " + goodid);
             //商品库存量减少
             if(goodsService.updateGoodsByGoodId(goodid)){
                 //用户购物车商品数增加
                 shopCarService.updateUserIdAndGoodId(userid,goodid);
-            }else{
-                out.println("库存量不足");
+                flag1 = true;
+            } else {
+                flag2 = false;
+//                out.println("库存量不足");
             }
         }
+        if (flag1 && flag2) {
+            object.put("error", "1");
+        } else {
+            object.put("error", "0");
+        }
+        out.println(object.toString());
+
+        out.flush();
         out.close();
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("do post");
         doGet(request,response);
     }
 }
--- src/Servlet/ShopCarServlet.java
+++ src/Servlet/ShopCarServlet.java
@@ -1,9 +1,12 @@
 package Servlet;
 
+import Entity.Goods;
 import Entity.ShopCar;
 import Service.ShopCarService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,19 +14,37 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShopCarServlet {
+@WebServlet(name = "Servlet.ShopCarServlet", urlPatterns = "/shop_car")
+public class ShopCarServlet  extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
         response.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
 
         String userid = request.getSession().getAttribute("userid").toString();
+        System.out.println("userId: " + userid);
         //根据用户ID获得该用户购物车的所有信息
         ShopCarService shopCarService = new ShopCarService();
-        List<ShopCar> shopCars = new ArrayList<>();
-        shopCars = shopCarService.queryAllGoods(userid);
-        request.setAttribute("shopcar",shopCars);
+        List<ShopCar> goods = new ArrayList<>();
+        goods = shopCarService.queryAllGoods(userid);
 
+        System.out.println("goods: " + goods);
+        // 生成购物车
+        String json = new String();
+        json += "{\"items\":[";
+        for (int i = 0; i < goods.size(); ++i) {
+            json += "{\"GoodId\":\"" + goods.get(i).getGoodId() + "\",\""
+                    + "UserId\":\"" + goods.get(i).getUserId() + "\",\""
+                    + "Number\":\"" + goods.get(i).getNume() + "\",\""
+                    + "Price\":\"" + goods.get(i).getPrice() + "\""
+                    +"}";
+            if (i != goods.size() - 1) json += ",";
+        }
+        json += "], \"error\": 1}";
+        System.out.print(json);
+
+        out.println(json);
+        out.flush();
         out.close();
     }
 
